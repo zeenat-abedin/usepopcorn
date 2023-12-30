2e68a76ce12490510df8f2bb7fd0565a93f51119
zeenat-abedin <zeenu.23@gmail.com>
2023-12-30 21:49:45 +0530

    Update StarRating component to accept color and size props

--- src/StarRating.jsx
+++ src/StarRating.jsx
@@ -11,20 +11,16 @@ const starContainerStyle = {
   gap: "4px",
 };
 
-const textStyle = {
-  lineHeight: "1",
-  margin: "0",
-};
-
-const starStyle = {
-  width: "48px",
-  height: "48px",
-  display: "block",
-};
-
-function StarRating({ maxRating }) {
+function StarRating({ maxRating = 5, color = "#fcc419", size = 48 }) {
   const [rating, setRating] = useState(0);
   const [tempRating, setTempRating] = useState(0);
+
+  const textStyle = {
+    lineHeight: "1",
+    margin: "0",
+    color,
+    fontSize: `${size / 1.5}px`,
+  };
 
   function handleRating(rating) {
     setRating(rating);
@@ -40,6 +36,8 @@ function StarRating({ maxRating }) {
             onRate={() => handleRating(i + 1)}
             onHoverIn={() => setTempRating(i + 1)}
             onHoverOut={() => setTempRating(0)}
+            size={size}
+            color={color}
           />
         ))}
       </div>
@@ -50,7 +48,12 @@ function StarRating({ maxRating }) {
 
 export default StarRating;
 
-function Star({ onRate, full, onHoverIn, onHoverOut }) {
+function Star({ onRate, full, onHoverIn, onHoverOut, size, color }) {
+  const starStyle = {
+    width: `${size}px`,
+    height: `${size}px`,
+    display: "block",
+  };
   return (
     <span
       role="button"
@@ -63,8 +66,8 @@ function Star({ onRate, full, onHoverIn, onHoverOut }) {
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
-          fill="#fff"
-          stroke="#000"
+          fill={color}
+          stroke={color}
         >
           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
         </svg>
