70588ce12e12c1181fc27b2871d9af05b2fa0277
zeenat-abedin <zeenu.23@gmail.com>
2023-12-30 19:02:38 +0530

    Refactor ListBox and WatchedBox components to Box component

--- src/App.js
+++ src/App.js
@@ -93,17 +93,14 @@ function Main({ children }) {
   return <main className="main">{children}</main>;
 }
 
-function ListBox({ children }) {
-  const [isOpen1, setIsOpen1] = useState(true);
+function Box({ children }) {
+  const [isOpen, setIsOpen] = useState(true);
   return (
     <div className="box">
-      <button
-        className="btn-toggle"
-        onClick={() => setIsOpen1((open) => !open)}
-      >
-        {isOpen1 ? "–" : "+"}
+      <button className="btn-toggle" onClick={() => setIsOpen((open) => !open)}>
+        {isOpen ? "–" : "+"}
       </button>
-      {isOpen1 && children}
+      {isOpen && children}
     </div>
   );
 }
@@ -132,22 +129,6 @@ function Movie({ movie }) {
         </p>
       </div>
     </li>
-  );
-}
-
-function WatchedBox({ children }) {
-  const [isOpen2, setIsOpen2] = useState(true);
-
-  return (
-    <div className="box">
-      <button
-        className="btn-toggle"
-        onClick={() => setIsOpen2((open) => !open)}
-      >
-        {isOpen2 ? "–" : "+"}
-      </button>
-      {isOpen2 && children}
-    </div>
   );
 }
 
@@ -220,14 +201,13 @@ export default function App() {
       </Navbar>
 
       <Main>
-        <ListBox>
+        <Box>
           <MovieList movies={movies} />
-        </ListBox>
-
-        <WatchedBox>
+        </Box>
+        <Box>
           <WatchedSummary watched={watched} />
           <WatchedList watched={watched} />
-        </WatchedBox>
+        </Box>
       </Main>
     </>
   );
