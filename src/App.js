8141308bbb03af7d35b1c1c1288c47c51f027293
zeenat-abedin <zeenu.23@gmail.com>
2024-01-04 18:34:38 +0530

    Added a star rating component to the movie details page. Also added loading state and updated the API key

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
+import StarRating from "./StarRating";
 
 const KEY = "1c1ab797";
+// const KEY = "f84fc31d";
 
 const tempMovieData = [
   {
@@ -132,32 +134,35 @@ function Movie({ movie, onSelectMovie }) {
 
 function MovieDetails({ selectedId, onCloseMovie }) {
   const [movie, setMovie] = useState({});
-
+  const [isLoading, setIsLoading] = useState(false);
   const {
     Poster: poster,
     Title: title,
     Type: type,
     Year: year,
     imdbID,
-    Released: released,
     Runtime: runtime,
-    Genre: genre,
     imdbRating,
-    Director: director,
-    Actors: actors,
     Plot: plot,
+    Released: released,
+    Actors: actors,
+    Director: director,
+    Genre: genre,
   } = movie;
 
+  console.log({ title, year });
   useEffect(() => {
     async function getMovieDetails() {
+      setIsLoading(true);
       const res = await fetch(
         `https://www.omdbapi.com/?i=${selectedId}=${KEY}&i=${selectedId}`
       );
       const data = await res.json();
       setMovie(data);
+      setIsLoading(false);
     }
     getMovieDetails();
-  }, []);
+  }, [selectedId]);
 
   return (
     <div className="details">
@@ -178,7 +183,11 @@ function MovieDetails({ selectedId, onCloseMovie }) {
           </p>
         </div>
       </header>
+
       <section>
+        <div className="rating">
+          <StarRating maxRating={10} size={24} />
+        </div>
         <p>
           <em>{plot}</em>
         </p>
