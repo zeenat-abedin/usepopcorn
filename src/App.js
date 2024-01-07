483470e83a47288a9438065dbb0bfefe1ad2272e
zeenat-abedin <zeenu.23@gmail.com>
2024-01-07 23:33:55 +0530

    Renamed `watched` to `movie` in `WatchedMovie` component

--- src/App.js
+++ src/App.js
@@ -295,37 +295,33 @@ function WatchedMoviesList({ watched, onDeleteWatched }) {
   );
 }
 
-function WatchedMovie({ watched, onDeleteWatched }) {
+function WatchedMovie({ movie, onDeleteWatched }) {
   return (
-    <ul className="list">
-      {watched.map((movie) => (
-        <li key={movie.imdbID}>
-          <img src={movie.poster} alt={`${movie.title} poster`} />
-          <h3>{movie.Title}</h3>
-          <div>
-            <p>
-              <span>⭐️</span>
-              <span>{movie.imdbRating}</span>
-            </p>
-            <p>
-              <span>🌟</span>
-              <span>{movie.userRating}</span>
-            </p>
-            <p>
-              <span>⏳</span>
-              <span>{movie.runtime} min</span>
-            </p>
+    <li>
+      <img src={movie.poster} alt={`${movie.title} poster`} />
+      <h3>{movie.Title}</h3>
+      <div>
+        <p>
+          <span>⭐️</span>
+          <span>{movie.imdbRating}</span>
+        </p>
+        <p>
+          <span>🌟</span>
+          <span>{movie.userRating}</span>
+        </p>
+        <p>
+          <span>⏳</span>
+          <span>{movie.runtime} min</span>
+        </p>
 
-            <button
-              className="btn-delete"
-              onClick={() => onDeleteWatched(movie.imdbID)}
-            >
-              X
-            </button>
-          </div>
-        </li>
-      ))}
-    </ul>
+        <button
+          className="btn-delete"
+          onClick={() => onDeleteWatched(movie.imdbID)}
+        >
+          X
+        </button>
+      </div>
+    </li>
   );
 }
 
